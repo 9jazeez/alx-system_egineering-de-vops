b661dd7527b0bb042c9895fcac4fa8bba5ff4c6a
Lawal Azeez <9jazeez@gmail.com>
2022-01-06 19:04:56 +0000

    error corrected

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 #Comment for puppet file
 
-sshkey { 'ubuntu': 
-  name => ubuntu@918-web-01
-  ensure => present
-  options => {
-    'PasswordAuthentication' => 'no'
-    'IdentityFile' => '~/.ssh/school'
-    'PubKeyAuthentication' => 'yes'
-     	}
-  provider => ssh-rsa
-  target => ~/.ssh/school
-  type => ssh-rsa
-} 
+ssh_authorized_key { 'ubuntu@918-web-01':
+  ensure   => present,
+  options  => {
+    'PasswordAuthentication' => 'no',
+    'IdentityFile'           => '~/.ssh/school',
+    'PubKeyAuthentication'   => 'yes'
+       },
+  provider => ssh-rsa,
+  user     => 'ubuntu',
+  target   => '~/.ssh/school',
+  type     => ssh-rsa
+}
